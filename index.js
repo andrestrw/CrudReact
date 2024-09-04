69d82f560088313e97838e03449929a2ecb21c5c
andrestrw <133chicagobulls@gmail.com>
2024-09-04 19:47:45 +0200

    Test

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ app.use(express.json());
 
 app.use(express.static("dist"));
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
+// Habilitar CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(URL_CONNECT)
